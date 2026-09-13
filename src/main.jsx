f6ea80baa9fe079c007a9225a7bd434325407481
Crodyto <crodyto@gmail.com>
2026-09-13 23:56:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// PWA Service Worker Register kora hocche
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
