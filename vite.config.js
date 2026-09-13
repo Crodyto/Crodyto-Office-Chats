06bec8879a9d4c2a678a784030422f0697e8095f
Crodyto <crodyto@gmail.com>
2026-09-13 23:50:37 +0530

    Configure Vite for PWA support
    Added PWA support with VitePWA plugin and configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatic update hobe notun code push korle
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Crodyto Chat',
+        short_name: 'Crodyto',
+        description: 'Secure communication platform for Crodyto Employees',
+        theme_color: '#00a884', /* WhatsApp er green color */
+        background_color: '#efeae2',
+        display: 'standalone', /* Browser hide kore app er moto open hobe */
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
