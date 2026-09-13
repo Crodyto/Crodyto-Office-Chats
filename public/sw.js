2a2057de30238582bc5770f021aa2054412c0f1f
Crodyto <crodyto@gmail.com>
2026-09-14 01:15:05 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+// Background Push Notification Handler
+self.addEventListener('push', function(event) {
+  let data = { title: 'New Message', body: 'You have a new message' };
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  const options = {
+    body: data.body,
+    icon: '/icon-192x192.png',
+    badge: '/icon-192x192.png',
+    vibrate: [200, 100, 200]
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
+});
